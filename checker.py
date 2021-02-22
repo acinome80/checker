7cb0e0dfb07b0841bc22badec08ec3bdb265e90d
krisan-m <km.krisan@gmail.com>
2021-02-21 22:00:40 -0500

    fix driver

--- checker.py
+++ checker.py
@@ -3,7 +3,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 # Best buy urls:
 bb_url = "https://www.bestbuy.ca/en-ca/product/asus-rog-zephyrus-g15-15-6-gaming-laptop-grey-amd-ryzen-9-5900hs-1tb-ssd-16gb-ram-rtx-3060-eng/15264484"
@@ -23,8 +23,8 @@ else:
     # chrome_options.add_argument("--kiosk") # use this for debugging on Linux/Mac
     chrome_options.add_argument("--window-size=3072,1920") # use this for debugging on Windows 3072 x 1920
 
-# driver = webdriver.Chrome(os.getenv("WEBDRIVER_PATH"), options=chrome_options)
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(os.getenv("WEBDRIVER_PATH"), options=chrome_options)
+# driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.get(bb_url)
 
 try:
